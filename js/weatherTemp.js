0b093684e14b906e7898c7d2d45fd256bdd22093
nusnishat <74313809+nusnishat@users.noreply.github.com>
2021-08-28 11:41:30 +0600

    first commit

---
+++ js/weatherTemp.js
@@ -0,0 +1,29 @@
+const weatherLoad = () =>{
+    // get the searched text
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    // api connection
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${searchText}&appid=075a7af654343d5a13026226dc0a891b`)
+    .then(response => response.json())
+    .then(data => weatherDisplay(data,searchText))
+    .catch(error => errorDisplay());
+    // clean the searchField
+    searchField.value = '';
+}
+
+const weatherDisplay = (weather, city) =>{
+    const weatherDisplayField = document.getElementById('wather-display');
+    weatherDisplayField.textContent = '';
+
+    const div = document.createElement('div');
+    div.classList.add('text-center','text-white');
+    div.innerHTML= `
+      <h3 class="fs-1 text-capitalize">${city}</h3>
+      <h3 class="fs-3">${weather.wind.deg}°F</h3>
+      <p class="fs-5">${weather.weather[0].main}</p>
+    `
+    weatherDisplayField.appendChild(div);
+}
+const errorDisplay = () => {
+    alert('Somenthing went wrong. Try with a valid city name.');
+}
